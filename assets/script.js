3a564cf9e9a5cadc2d8fd4b33ce56e579f073159
Charliy Vaughan <cvaug5@gmail.com>
2023-02-06 20:17:06 +0000

    added js file

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+
+
+
+
+// Declare variable for search button
+let searchButton = document.getElementById("search-button")
+let searchInput = document.getElementById("search-input")
+
+// Adding event listener to search button
+searchButton.addEventListener("click", function() { alert ("Search Button has been clicked!");});
