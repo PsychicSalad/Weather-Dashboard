73ca6a4893344e42fb5f15ffa2f8325761731116
Charliy Vaughan <cvaug5@gmail.com>
2023-02-07 21:08:20 +0000

    started working on js file

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,32 @@
+// $(document).ready(function () {
+//   var apiUrl =
+//     "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={7c6e84c4d2bbe1fd8c0477ebcc15c4ba}";
+// });
 
+// my api key for openweathermap
+// "7c6e84c4d2bbe1fd8c0477ebcc15c4ba"
 
+// Declare variable for form, button and input
+// let searchButton = document.getElementById("search-button");
+// let searchInput = document.getElementById("search-input");
+// let searchForm = document.getElementById("search-form");
 
+// let searchButton = $("search-button");
+let searchInput = $("search-input");
+let searchForm = $("search-form");
 
-// Declare variable for search button
-let searchButton = document.getElementById("search-button")
-let searchInput = document.getElementById("search-input")
+// Adding event listener to search form
 
-// Adding event listener to search button
-searchButton.addEventListener("click", function() { alert ("Search Button has been clicked!");});
+function submitSearchFrom(event) {
+  event.preventDefault();
+  alert(searchInput.val().trim());
+}
+
+searchForm.on("submit", submitSearchFrom);
+
+// searchForm.addEventListener("submit", function submitForm(event) {
+//   event.preventDefault();
+//   console.log(searchInput.val());
+// });
+
+// submitForm();
